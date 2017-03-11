f5e1211efa6e71e7e7837969eb00bd83ab495445
Nikita <nickita1@gmail.com>
2017-03-12 01:57:22 +0200

    initial commit for home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServiceComponent } from './service/service.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiceComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
